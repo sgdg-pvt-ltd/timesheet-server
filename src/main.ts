664ea904dbbc8702d72d2201ecf691c4be32adcf
unknown <ASUS@SG-LAP-12.sgdesign.com>
2024-06-20 20:09:18 +0545

    fixes

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.LOCAL_PORT);
 }
 bootstrap();
